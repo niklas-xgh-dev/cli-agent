150a15fe301ca390caee6a3cb914631607640a25
niklas-xgh-dev <techstag@outlook.com>
2025-08-01 12:46:02 +0200

    implemented verbose parameter to help debugging

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 import sys
 
 load_dotenv()
@@ -14,16 +15,33 @@ def main():
     if len(sys.argv) == 1:
         print("No argument/user prompt given")
         sys.exit(1)
+    elif "--verbose" in sys.argv: 
+        verbose=True
+        user_prompt = sys.argv[1]
+    else:
+        verbose=False
+        user_prompt = sys.argv[1]
     #try :
     #   user_prompt = sys.argv[1]
     #except:
     #    user_prompt = input("enter your promt:")
     #user_prompt = sys.argv[1]
-    response = client.models.generate_content(model='gemini-2.0-flash', contents=sys.argv[1])
+
+    messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+
+    response = client.models.generate_content(
+        model='gemini-2.0-flash', 
+        contents=sys.argv[1]
+        )
     #print(response)
     print(f"Response: {response.text}")#, response.prompt_token_count)
-    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
 
 if __name__ == "__main__":
     main()
